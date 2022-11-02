3b8777b90f7631a6cb3773710106f4b54c799519
Kidong Lee <mykidong@gmail.com>
2022-11-02 17:12:18 +0900

    remove ad2...

--- examples/container_engine/main.tf
+++ examples/container_engine/main.tf
@@ -54,10 +54,10 @@ data "oci_identity_availability_domain" "ad1" {
   ad_number      = 1
 }
 
-data "oci_identity_availability_domain" "ad2" {
-  compartment_id = var.tenancy_ocid
-  ad_number      = 2
-}
+#data "oci_identity_availability_domain" "ad2" {
+#  compartment_id = var.tenancy_ocid
+#  ad_number      = 2
+#}
 
 data "oci_kms_vault" "test_vault" {
   #Required
@@ -113,18 +113,18 @@ resource "oci_core_subnet" "clusterSubnet_1" {
   route_table_id    = oci_core_route_table.test_route_table.id
 }
 
-resource "oci_core_subnet" "clusterSubnet_2" {
-  #Required
-  availability_domain = data.oci_identity_availability_domain.ad2.name
-  cidr_block          = "10.0.21.0/24"
-  compartment_id      = var.compartment_ocid
-  vcn_id              = oci_core_vcn.test_vcn.id
-  display_name        = "tfSubNet1ForClusters"
-
-  # Provider code tries to maintain compatibility with old versions.
-  security_list_ids = [oci_core_vcn.test_vcn.default_security_list_id]
-  route_table_id    = oci_core_route_table.test_route_table.id
-}
+#resource "oci_core_subnet" "clusterSubnet_2" {
+#  #Required
+#  availability_domain = data.oci_identity_availability_domain.ad2.name
+#  cidr_block          = "10.0.21.0/24"
+#  compartment_id      = var.compartment_ocid
+#  vcn_id              = oci_core_vcn.test_vcn.id
+#  display_name        = "tfSubNet1ForClusters"
+#
+#  # Provider code tries to maintain compatibility with old versions.
+#  security_list_ids = [oci_core_vcn.test_vcn.default_security_list_id]
+#  route_table_id    = oci_core_route_table.test_route_table.id
+#}
 
 resource "oci_core_subnet" "nodePool_Subnet_1" {
   #Required
@@ -139,18 +139,18 @@ resource "oci_core_subnet" "nodePool_Subnet_1" {
   route_table_id    = oci_core_route_table.test_route_table.id
 }
 
-resource "oci_core_subnet" "nodePool_Subnet_2" {
-  #Required
-  availability_domain = data.oci_identity_availability_domain.ad2.name
-  cidr_block          = "10.0.23.0/24"
-  compartment_id      = var.compartment_ocid
-  vcn_id              = oci_core_vcn.test_vcn.id
-
-  # Provider code tries to maintain compatibility with old versions.
-  security_list_ids = [oci_core_vcn.test_vcn.default_security_list_id]
-  display_name      = "tfSubNet2ForNodePool"
-  route_table_id    = oci_core_route_table.test_route_table.id
-}
+#resource "oci_core_subnet" "nodePool_Subnet_2" {
+#  #Required
+#  availability_domain = data.oci_identity_availability_domain.ad2.name
+#  cidr_block          = "10.0.23.0/24"
+#  compartment_id      = var.compartment_ocid
+#  vcn_id              = oci_core_vcn.test_vcn.id
+#
+#  # Provider code tries to maintain compatibility with old versions.
+#  security_list_ids = [oci_core_vcn.test_vcn.default_security_list_id]
+#  display_name      = "tfSubNet2ForNodePool"
+#  route_table_id    = oci_core_route_table.test_route_table.id
+#}
 
 resource "oci_containerengine_cluster" "test_cluster" {
   #Required
@@ -169,7 +169,8 @@ resource "oci_containerengine_cluster" "test_cluster" {
 
   #Optional
   options {
-    service_lb_subnet_ids = [oci_core_subnet.clusterSubnet_1.id, oci_core_subnet.clusterSubnet_2.id]
+    #service_lb_subnet_ids = [oci_core_subnet.clusterSubnet_1.id, oci_core_subnet.clusterSubnet_2.id]
+    service_lb_subnet_ids = [oci_core_subnet.clusterSubnet_1.id]
 
     #Optional
     add_ons {
@@ -177,6 +178,7 @@ resource "oci_containerengine_cluster" "test_cluster" {
       is_kubernetes_dashboard_enabled = "true"
       is_tiller_enabled               = "true"
     }
+
 
     admission_controller_options {
       #Optional
@@ -198,7 +200,8 @@ resource "oci_containerengine_node_pool" "test_node_pool" {
   kubernetes_version = data.oci_containerengine_node_pool_option.test_node_pool_option.kubernetes_versions[0]
   name               = "tfPool"
   node_shape         = "VM.Standard2.1"
-  subnet_ids         = [oci_core_subnet.nodePool_Subnet_1.id, oci_core_subnet.nodePool_Subnet_2.id]
+  #subnet_ids         = [oci_core_subnet.nodePool_Subnet_1.id, oci_core_subnet.nodePool_Subnet_2.id]
+  subnet_ids         = [oci_core_subnet.nodePool_Subnet_1.id]
 
   #Optional
   initial_node_labels {
@@ -233,7 +236,8 @@ resource "oci_containerengine_node_pool" "test_flex_shape_node_pool" {
   kubernetes_version = data.oci_containerengine_node_pool_option.test_node_pool_option.kubernetes_versions[0]
   name               = "flexShapePool"
   node_shape         = "VM.Standard.E3.Flex"
-  subnet_ids         = [oci_core_subnet.nodePool_Subnet_1.id, oci_core_subnet.nodePool_Subnet_2.id]
+  #subnet_ids         = [oci_core_subnet.nodePool_Subnet_1.id, oci_core_subnet.nodePool_Subnet_2.id]
+  subnet_ids         = [oci_core_subnet.nodePool_Subnet_1.id]
 
   node_source_details {
     #Required
